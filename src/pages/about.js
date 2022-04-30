d3980502f17b0c1ad60a9eb01bcfed14ea0872ef
newermore <Newermore@mail.com>
2022-04-30 19:04:23 +0500

    commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,16 +5,16 @@ function About() {
   return (
     <Layout>
       <section>
-        <div className=" w-full  text-white py-20">
-          <div className="container  flex flex-col md:flex-row items-center my-12 md:my-24">
+        <div className=" w-full  text-white md:pt-10 pt-5">
+          <div className="container  flex flex-col md:flex-row md:items-start my-12 md:my-24">
             <div className="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
-              <h1 className="text-3xl md:text-5xl p-2 text-yellow-300 tracking-loose">
+              <h1 className="text-3xl md:text-5xl  text-yellow-300 tracking-loose">
                 Another Travel
               </h1>
-              <h2 className="text-3xl md:text-5xl text-gray-600 leading-relaxed md:leading-snug mb-2">
+              <h2 className="text-3xl md:text-5xl md:my-16 text-gray-600 leading-relaxed md:leading-snug mb-2">
                 Turistik kompaniya
               </h2>
-              <p className="text-sm md:text-base text-gray-600 mb-4">
+              <p className="text-sm md:text-base text-gray-600 mb-10">
                 Bizning kompaniya ichki va tashqituristlarni kutib olish va
                 kuzatib qoyish blan shug'ullanadi
               </p>
@@ -26,7 +26,7 @@ function About() {
               </Link>
             </div>
             <div className="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
-              <div className="h-48 flex flex-wrap content-center">
+              <div className="h-full flex flex-wrap content-center">
                 <div>
                   <img
                     className=" rounded-3xl mt-5 w-full md:mt-0  md:p-0"
@@ -34,6 +34,29 @@ function About() {
                   />
                 </div>
               </div>
+              <p className="text-md md:text-xl mt-5  text-gray-700 ">
+                Toshkent – O’zbekistonning poytaxti va Markaziy Osiyodagi eng
+                katta shaharlardan biri. Olis o’tmishdagidek hozirgi paytda ham
+                Toshkent o’zimizning O’zbekiston Havo Yo’llari va xalqaro
+                aviakompaniyalar yordamida xalqaro transport yo’nalishlarining
+                chorrahasidir. Toshkent dunyoning eng qadimiy shaharlaridan
+                biridir. 2009-yilda u o’zining 2200 yilligini nishonladi. Bir
+                paytning o’zida Toshkent zamonaviy ishlab chiqarish markazidir,
+                bu yerda 500 dan ortiq kompaniyalar turli xil mahsulotlar;
+                samolyotdan traktorgacha, televizordan to`qimachilik va oyoq
+                kiyimlargacha ishlab chiqaradi. Toshkent tez sur`atlarda tarixiy
+                yodgorliklar va zamonaviy osmono’par binolarning o’zaro
+                uyg’unligida zamonaviy rivojlangan xalqaro megapolisga
+                aylanmoqda. Bugungi kunda O’zbekistonda yuzga yaqin muzeylar
+                ishlab turibdi va ularning yarmi mamlakatning poytaxtida
+                joylashgan. Ularning har biri o’zbek xalqining boy madaniy
+                merosini aks ettiradi. Poytaxt markazida Temuriylar tarixi
+                Davlat Muzeyi joylashgan bo’lib, uning ulkan ko’k gumbazi
+                Samarqandning qadimiy gumbazini eslatadi. Uning eksponatlari
+                O’zbekiston hududida o’rta asrlardagi davlatchilik shakllarini
+                shakllanishidan guvohlik beradi va Amir Temur davridagi fan,
+                madaniyat va san’atning rivojlanishini aks ettiradi.
+              </p>
             </div>
           </div>
         </div>
